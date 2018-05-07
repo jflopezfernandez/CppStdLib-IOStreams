a94224c699ec7ff537e8b925bd9f4628178f70f7
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2018-05-07 07:41:53 -0400

    Initial commit

--- cppstdlib-iostreams/main.cpp
+++ cppstdlib-iostreams/main.cpp
@@ -1,8 +1,16 @@
 
 #include <iostream>
 
+template <typename T, typename Traits>
+std::basic_ostream<T, Traits>&
+NL(std::basic_ostream<T, Traits>& out)
+{
+    return out << out.widen('\n');
+}
+
 
 int main()
 {
-    std::cout << "testing...\n";
+    std::cout << "testing..." << NL;
+    std::cout << "new line char..." << NL;
 }
