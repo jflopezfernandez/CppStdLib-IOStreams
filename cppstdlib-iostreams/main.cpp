1b9803a0bd1d30c0ca774de89a0cdfc8a8c4db5c
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2018-05-07 08:38:42 -0400

    Experimenting with the `strtok` function. For some reason, my implementation is working fine as long as the `const char *` is set at compile time. If I try to actually read in the input from the command line, the program will print the first token value, then return from main.

--- cppstdlib-iostreams/main.cpp
+++ cppstdlib-iostreams/main.cpp
@@ -1,16 +1,64 @@
 
-#include <iostream>
+#define _CRT_SECURE_NO_WARNINGS
 
-template <typename T, typename Traits>
+#include <cstdlib>
+
+#include <fstream>
+#include <iomanip>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <sstream>
+#include <streambuf>
+#include <string>
+#include <string_view>
+#include <vector>
+
+template <typename T, typename Traits = char_traits<T>>
 std::basic_ostream<T, Traits>&
 NL(std::basic_ostream<T, Traits>& out)
 {
     return out << out.widen('\n');
 }
 
+template <typename T>
+void PrintVector(const std::vector<T>& vector)
+{
+    for (auto& v : vector)
+    {
+        std::cout << v << NL;
+    }
+}
+
 
 int main()
 {
-    std::cout << "testing..." << NL;
-    std::cout << "new line char..." << NL;
+    std::vector<std::string> words;
+
+    //char input[128] = { 0 };
+    char input[] = "Jose Fernando Lopez Fernandez Gomez Gutierrez";
+
+    /*std::cout << "Enter message: ";
+    std::cin >> std::setw(128) >> input;
+
+    if (std::cin.fail())
+    {
+        std::cerr << "[Error]: Failed to read user input." << NL;
+
+        return EXIT_FAILURE;
+    }*/
+
+    char *singleWord = std::strtok(input, " ");
+
+    while (singleWord)
+    {
+        words.emplace_back(singleWord);
+
+        singleWord = std::strtok(nullptr, " ");
+    }
+
+    PrintVector(words);
+
+    return EXIT_SUCCESS;
 }
